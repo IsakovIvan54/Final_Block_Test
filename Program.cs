436637b3da850e28a3a8211c8dc676595f9758d0
IvanIsakov <wildcs702@gmail.com>
2023-07-14 20:08:38 +0300

    add example in program

--- Program.cs
+++ Program.cs
@@ -1,5 +1,11 @@
-﻿string[] array1 = new string[5] {"123", "23", "hello", "world", "res"};
-string[] array2 = new string[array1.Length];
+﻿string[] arrayIn1 = new string[4] {"Hello", "2", "world", ":-)"};
+string[] arrayIn2 = new string[4] {"1234", "1567", "-2", "computer science"};
+string[] arrayIn3 = new string[3] {"Russia", "Denmark", "Kazan"};
+
+string[] arrayOut1 = new string[arrayIn1.Length];
+string[] arrayOut2 = new string[arrayIn2.Length];
+string[] arrayOut3 = new string[arrayIn3.Length];
+
 void SecondArrayWithIF(string[] array1, string[] array2)
 {
     int count = 0;
@@ -20,5 +26,12 @@ void PrintArray(string[] array)
     }
     Console.WriteLine();
 }
-SecondArrayWithIF(array1, array2);
-PrintArray(array2);
+
+SecondArrayWithIF(arrayIn1, arrayOut1);
+PrintArray(arrayOut1);
+
+SecondArrayWithIF(arrayIn2, arrayOut2);
+PrintArray(arrayOut2);
+
+SecondArrayWithIF(arrayIn3, arrayOut3);
+PrintArray(arrayOut3);
